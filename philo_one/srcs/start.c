87f9ac55e77479173d850cce8f402d338ab9b925
Касим Байбиков <kasim.baybikov@gmail.com>
2021-04-13 19:55:32 +0300

    create timer

--- philo_one/srcs/start.c
+++ philo_one/srcs/start.c
@@ -1,14 +1,19 @@
 #include "philo_one.h"
 
-void	start(t_all *all)
+unsigned long		my_time()
 {
 	struct timeval	t_time;
 
+	gettimeofday(&t_time, 0);
+	return ((t_time.tv_sec * 1000) + (t_time.tv_usec / 10000));
+}
+
+void				start(t_all *all)
+{
+	unsigned long start = my_time();
 	while (1)
 	{
-		gettimeofday(&t_time, 0);
-		printf("%d\n", t_time.tv_usec);
-		break;
+		printf("%lu\n", my_time() - start);
 		//printf("%ld\n", t_time.tv_sec);
 	}
 	(void)all;
